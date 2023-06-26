30d976dc5566397d1c003e30bfeed611697aab7e
= <=>
2023-06-26 15:22:48 +0800

    use deprecated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import { Radio } from './components/Radio';
 
 function App() {
   const [highlightColor, setHighlightColor] = useState('green');
+  const container = useRef(null);
+
+  const _ = () => {
+    if (!container.current) return;
+
+    const highlighted = [...container.current.getElementsByTagName('span')];
+
+    const data = { like: [], dislike: [], doNotUnderstand: [] };
+
+    highlighted.forEach((span) => {
+      switch (span.style.backgroundColor) {
+        case 'green':
+          data.like.push(span.textContent);
+          return;
+        case 'red':
+          data.dislike.push(span.textContent);
+          return;
+        case 'yellow':
+          data.doNotUnderstand.push(span.textContent);
+          return;
+      }
+    });
+
+    console.log(data);
+  };
 
   const handleHighlight = () => {
     const highlighted = window.getSelection();
-    if (!highlighted.toString()) return;
+    if (!highlighted.toString().replace(/\s/g, '')) return;
 
     const range = highlighted.getRangeAt(0);
 
-    const span = document.createElement('span');
-    span.style.backgroundColor = highlightColor;
+    document.designMode = 'on';
 
-    try {
-      range.surroundContents(span);
-    } catch (err) {
-      console.log(range);
+    if (range) {
+      console.log(highlighted.anchorOffset, range.startOffset, range.endOffset);
+
+      console.log(highlighted.anchorOffset);
+
+      highlighted.removeAllRanges();
+      highlighted.addRange(range);
     }
+
+    document.execCommand('backColor', false, highlightColor);
+    document.designMode = 'off';
+
+    _();
 
     window.getSelection().removeAllRanges();
   };
@@ -29,11 +61,13 @@ function App() {
       onChange={(evt) => setHighlightColor(evt.target.value)}
     >
       <div style={{ display: 'flex', gap: 16, alignItems: 'center' }}>
-        <Radio name="type" value="green" label="Like" />
-        <Radio name="type" value="red" label="Dislike" />
-        <Radio name="type" value="yellow" label="Do not understand" />
+        <Radio name="action" value="green" label="Like" />
+        <Radio name="action" value="red" label="Dislike" />
+        <Radio name="action" value="yellow" label="Do not understand" />
+        <Radio name="action" value="transparent" label="Erase" />
       </div>
       <div
+        ref={container}
         onMouseUp={handleHighlight}
         dangerouslySetInnerHTML={{
           __html:
